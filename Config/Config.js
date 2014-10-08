a6a1d057d079850c4c665fae8946ee30d7e09343
Griffin Ledingham <gcledingham@gmail.com>
2014-10-08 09:46:07 -0700

    Update Config.js

--- Config/Config.js
+++ Config/Config.js
@@ -1,9 +1,9 @@
 exports.file_1 = './Data/file1.csv';
-//place columns to pull out here
+//place columns to pull out here, comma separated
 exports.columns_1 = [];
 
 exports.file_2 = './Data/file2.csv';
-//place columns to pull out here
+//place columns to pull out here comma separated
 exports.columns_2 = [];
 
 exports.main_function = function(input1, input2){
