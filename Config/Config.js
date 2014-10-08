2a0a77f242d2da399204a6cd804f7979c0efba6d
Griffin Ledingham <gcledingham@gmail.com>
2014-10-08 09:44:25 -0700

    Initial commit.

---
+++ Config/Config.js
@@ -0,0 +1,11 @@
+exports.file_1 = './Data/file1.csv';
+//place columns to pull out here
+exports.columns_1 = [];
+
+exports.file_2 = './Data/file2.csv';
+//place columns to pull out here
+exports.columns_2 = [];
+
+exports.main_function = function(input1, input2){
+
+};
